ad93365bc5858aecc5b41080f323a2de0bf9d48c
ShimingZ <Shiming@cca.edu>
2017-05-04 16:05:24 -0700

    p3

---
+++ Exercises/Exercise-11/site/accounts/shiming.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: shiming
+password: >
+  $2a$10$CaeRY2kfpmw5uwLnomwR7.JC5183xZ1BYw0VwDM.ikOKoRxe/mNbe
+email: shiming@cca.edu
+language: en
+role: admin
+history:
+  - projects
